de4fe4fdabb07abc8a71b2d2cc4341c7edef818a
Marina Kuklina <nelmare@yandex.ru>
2020-11-06 16:46:07 +0300

    Prepare for demo

--- js/burger.js
+++ js/burger.js
@@ -1,5 +1,7 @@
+var body = document.querySelector('.body');
 var navMainList = document.querySelector('.main-nav__list');
 var navToggle = document.querySelector('.main-nav__toggle');
+
 
 navMainList.classList.remove('main-nav__list--nojs');
 navToggle.classList.remove('main-nav__toggle--nojs');
@@ -11,4 +13,5 @@ navToggle.addEventListener('click', function () {
   navMainList.classList.toggle('main-nav--open');
   navToggle.classList.toggle('main-nav__toggle--open');
   navToggle.classList.toggle('main-nav__toggle--closed');
+  body.classList.toggle('menu__open');
 });
