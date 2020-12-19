8577428be4df3e731b9205872c673dbfe144ca0e
Marina Kuklina <nelmare@yandex.ru>
2020-12-19 16:42:18 +0300

    Adjust item-page tabs

--- js/item-tabs.js
+++ js/item-tabs.js
@@ -3,13 +3,13 @@ function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
 
     // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("order__tabcontent");
+    tabcontent = document.getElementsByClassName("product__tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
 
     // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("order__tablink");
+    tablinks = document.getElementsByClassName("product__tablink");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
