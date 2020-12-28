32944b1fd8122ea1308b5bd87a70dc25f2587571
Marina Kuklina <nelmare@yandex.ru>
2020-12-28 21:13:59 +0300

    Adjust header and nav for all pages

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var express = require('express');
 var app = express();
 
 app.set('port', process.env.PORT || 80);
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/build'));
 app.listen(app.get('port'), function () {});
